5f5403fd624097f8eef24c2b364bcb160b221a98
Thiago Silva <thiagompta008@hotmail.com>
2026-01-02 12:51:03 -0300

    Exercicio de finanças Alura

---
+++ exe5.py
@@ -0,0 +1,5 @@
+despesas = float(input("Digite o total de despesas do mês R$:"))
+if despesas > 3000:
+    print('Atenção! Você ultrapassou o limite do orçamento.')
+else:
+    print('Você está dentro do orçamento.')
